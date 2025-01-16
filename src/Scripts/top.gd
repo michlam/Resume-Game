58d86c1b5b655f93a517b34811ece8ffde5525c5
UBC Student <lam.michael.81201@gmail.com>
2025-01-16 15:45:52 -0800

    Added arrows idle animation

--- src/Scripts/top.gd
+++ src/Scripts/top.gd
@@ -5,6 +5,11 @@ const DESCRIPTION_LIST = ["Projects", "Education", "Games", "Career", "Drawings"
 const BUTTON_SELECTED_BASE_X = 1367
 const BUTTON_SELECTED_OFFSET = 50
 
+func _ready():
+	$AnimationPlayer.play("arrows_idle")
+
+
 func update_focus():
 	$Description.set_text(DESCRIPTION_LIST[world.focus]);
 	$Buttons/ButtonSelected.position.x = BUTTON_SELECTED_BASE_X + world.focus * BUTTON_SELECTED_OFFSET
+	$AnimationPlayer.play("arrows_change")
